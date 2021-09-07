7f5d2b693d93ae52fff469f427fd034e2b943015
baikseungjeon <tmdwjsofficial@naver.com>
2021-09-07 22:08:35 +0900

    timeline

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.querySelector('.slide-btn1').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(0)';
+})
+
+document.querySelector('.slide-btn2').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(-100vw)';
+})
+
+document.querySelector('.slide-btn3').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(-200vw)';
+})
+
+document.querySelector('.slide-btn4').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(-300vw)';
+})
+
+document.querySelector('.slide-btn5').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(-400vw)';
+})
+
+document.querySelector('.slide-btn6').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(-500vw)';
+})
+
+document.querySelector('.slide-btn7').addEventListener('click', () => {
+    document.querySelector('.timeline-slide').style.transform = 'translateX(-600vw)';
+})
+
