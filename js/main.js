e760a2173b3a6bfb446a7813c5241a61dfc5a6e1
baikseungjeon <tmdwjsofficial@naver.com>
2021-09-29 22:41:10 +0900

    timeline

--- js/main.js
+++ js/main.js
@@ -43,3 +43,14 @@ document.querySelector('.slide-btn7').addEventListener('click', () => {
 
 //     console.log('currentScrollValue is ' + currentScrollValue);
 // });
+
+/* introduce 클릭 시 content-box 내용 보이기 */
+
+let introBtn = document.querySelector('.introduce-content-btn');
+let introBox = document.querySelector('.introduce-content-box');
+
+introBtn.addEventListener("click", () => {
+
+    introBox.classList.toggle('on');
+    
+});
