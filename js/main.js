81888e80d96f147ae3dd6051fd7dea8ed1691226
baikseungjeon <tmdwjsofficial@naver.com>
2021-11-27 22:53:50 +0900

    1127

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-/* introduce 클릭 시 content-box 내용 보이기 */
-
+// introduce 클릭 시 content-box 내용 보이기
 let introBtn = document.querySelector('.introduce-content-btn');
 let introBox = document.querySelector('.introduce-content-box');
 
 introBtn.addEventListener("click", () => {
     introBox.classList.toggle('on');
 });
+
+// swiper
+let swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
